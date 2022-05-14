35eefff97e696eada0f234b933193f5962b0a820
derodero24 <k.itsh20524abc@gmail.com>
2022-05-14 14:48:27 +0900

    move files

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite';
-
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   base: './',
   server: {
     open: true,
-    https: true,
+    // https: true,
   },
 });
