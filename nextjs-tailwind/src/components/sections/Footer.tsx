6956f212e287d4384b717204c4456718bf833430
derodero24 <k.itsh20524abc@gmail.com>
2022-08-13 21:08:17 +0900

    update footer

--- nextjs-tailwind/src/components/sections/Footer.tsx
+++ nextjs-tailwind/src/components/sections/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 px-6 py-2">
-      <div className="container mx-auto text-center text-white">
-        &copy; {new Date().getFullYear()} derodero24
-      </div>
+    <footer className="px-6 py-2 text-center">
+      &copy; {new Date().getFullYear()} derodero24
     </footer>
   );
 }
