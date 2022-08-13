c01485812cc3b3f8e2d2161c7eb32a86b969c30b
derodero24 <k.itsh20524abc@gmail.com>
2022-08-13 18:03:49 +0900

    remove some files

--- nextjs-tailwind/src/components/sections/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 px-6 py-2">
-      <div className="container mx-auto text-center text-white">
-        &copy; {new Date().getFullYear()} derodero24
-      </div>
-    </footer>
-  );
-}
