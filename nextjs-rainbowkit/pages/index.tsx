56197d3e26e956c840121e9ec6053a65a4e8eb50
derodero24 <k.itsh20524abc@gmail.com>
2022-10-04 12:50:37 +0900

    add nextjs-rainbowkit template

---
+++ nextjs-rainbowkit/pages/index.tsx
@@ -0,0 +1,22 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <main
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <ConnectButton />
+    </main>
+  );
+};
+
+export default Home;
