e38eff42f1d47674e2c4f4f7b7ab827d70c0cc60
derodero24 <k.itsh20524abc@gmail.com>
2022-10-10 14:03:16 +0900

    add custom button

--- nextjs-rainbowkit/src/pages/_app.tsx
+++ nextjs-rainbowkit/src/pages/_app.tsx
@@ -6,12 +6,10 @@ import {
   darkTheme,
   lightTheme,
 } from '@rainbow-me/rainbowkit';
-import { RainbowKitSiweNextAuthProvider } from '@rainbow-me/rainbowkit-siwe-next-auth';
-import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { WagmiConfig } from 'wagmi';
 
-import { chains, getSiweMessageOptions, wagmiClient } from '../lib/rainbowkit';
+import { chains, wagmiClient } from '../lib/rainbowkit';
 
 import type { Session } from 'next-auth';
 import type { AppProps } from 'next/app';
@@ -19,26 +17,26 @@ import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <ThemeProvider attribute="class">
-      <SessionProvider refetchInterval={0} session={pageProps.session}>
-        <WagmiConfig client={wagmiClient}>
-          <RainbowKitSiweNextAuthProvider
-            getSiweMessageOptions={getSiweMessageOptions}
-          >
-            <RainbowKitProvider
-              chains={chains}
-              // PC/ブラウザ全体のテーマ設定に応じて変化
-              theme={{
-                lightMode: lightTheme(),
-                darkMode: darkTheme(),
-              }}
-              // ウォレット選択時にアイコンが飛び出してくる
-              coolMode
-            >
-              <Component {...pageProps} />
-            </RainbowKitProvider>
-          </RainbowKitSiweNextAuthProvider>
-        </WagmiConfig>
-      </SessionProvider>{' '}
+      {/* <SessionProvider refetchInterval={0} session={pageProps.session}> */}
+      <WagmiConfig client={wagmiClient}>
+        {/* <RainbowKitSiweNextAuthProvider
+          getSiweMessageOptions={getSiweMessageOptions}
+        > */}
+        <RainbowKitProvider
+          chains={chains}
+          // PC/ブラウザ全体のテーマ設定に応じて変化
+          theme={{
+            lightMode: lightTheme(),
+            darkMode: darkTheme(),
+          }}
+          // ウォレット選択時にアイコンが飛び出してくる
+          coolMode
+        >
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+        {/* </RainbowKitSiweNextAuthProvider> */}
+      </WagmiConfig>
+      {/* </SessionProvider> */}
     </ThemeProvider>
   );
 }
--- nextjs-rainbowkit/src/pages/index.tsx
+++ nextjs-rainbowkit/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { unstable_getServerSession } from 'next-auth';
 import Head from 'next/head';
+import Image from 'next/image';
 
 import Layout from '../components/layouts/Layout';
 import { getAuthOptions } from './api/auth/[...nextauth]';
@@ -41,6 +42,88 @@ const Home: NextPage = () => {
         <ConnectButton showBalance={true} />
         <ConnectButton showBalance={false} />
         <ConnectButton label="Hello" />
+
+        <ConnectButton.Custom>
+          {({
+            account,
+            chain,
+            openAccountModal,
+            openChainModal,
+            openConnectModal,
+            mounted,
+          }) => {
+            return (
+              <div
+                {...(!mounted && {
+                  'aria-hidden': true,
+                  style: {
+                    opacity: 0,
+                    pointerEvents: 'none',
+                    userSelect: 'none',
+                  },
+                })}
+              >
+                {(() => {
+                  if (!mounted || !account || !chain) {
+                    return (
+                      <button onClick={openConnectModal} type="button">
+                        Connect Wallet
+                      </button>
+                    );
+                  }
+
+                  if (chain.unsupported) {
+                    return (
+                      <button onClick={openChainModal} type="button">
+                        Wrong network
+                      </button>
+                    );
+                  }
+
+                  return (
+                    <div style={{ display: 'flex', gap: 12 }}>
+                      <button
+                        onClick={openChainModal}
+                        style={{ display: 'flex', alignItems: 'center' }}
+                        type="button"
+                      >
+                        {chain.hasIcon && (
+                          <div
+                            style={{
+                              background: chain.iconBackground,
+                              width: 12,
+                              height: 12,
+                              borderRadius: 999,
+                              overflow: 'hidden',
+                              marginRight: 4,
+                            }}
+                          >
+                            {chain.iconUrl && (
+                              <Image
+                                alt={chain.name ?? 'Chain icon'}
+                                src={chain.iconUrl}
+                                width={12}
+                                height={12}
+                              />
+                            )}
+                          </div>
+                        )}
+                        {chain.name}
+                      </button>
+
+                      <button onClick={openAccountModal} type="button">
+                        {account.displayName}
+                        {account.displayBalance
+                          ? ` (${account.displayBalance})`
+                          : ''}
+                      </button>
+                    </div>
+                  );
+                })()}
+              </div>
+            );
+          }}
+        </ConnectButton.Custom>
       </main>
     </Layout>
   );
