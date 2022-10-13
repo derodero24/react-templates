27b193d7de2a81769df5ba378bd5671a96258925
derodero24 <k.itsh20524abc@gmail.com>
2022-10-13 12:28:14 +0900

    move main tag to Layout.ts

--- nextjs-tailwind/src/components/layouts/Layout.tsx
+++ nextjs-tailwind/src/components/layouts/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout(props: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      {props.children}
+      <main className="grow">{props.children}</main>
       <Footer />
     </div>
   );
