da98cd5d2768baf3015ba9913fe10ea835aee228
derodero24 <k.itsh20524abc@gmail.com>
2022-10-14 18:05:07 +0900

    update google font header

--- nextjs-tailwind/src/pages/_document.tsx
+++ nextjs-tailwind/src/pages/_document.tsx
@@ -10,6 +10,13 @@ export default function MyDocument() {
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
 
+        {/* Google Font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
