5b9b82da4a0752cfe20691e92854050ec8990fdb
derodero24 <k.itsh20524abc@gmail.com>
2022-10-14 18:03:09 +0900

    update icons

--- nextjs-tailwind/src/pages/_document.tsx
+++ nextjs-tailwind/src/pages/_document.tsx
@@ -4,7 +4,12 @@ export default function MyDocument() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        {/* icons */}
+        <link rel="icon" sizes="any" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
