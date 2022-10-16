fbd728d7a82c4bdc5ffb6951b47444e59ebcca9e
derodero24 <k.itsh20524abc@gmail.com>
2022-10-16 18:34:45 +0900

    add github link

--- nextjs-tailwind/src/components/sections/Header.tsx
+++ nextjs-tailwind/src/components/sections/Header.tsx
@@ -1,12 +1,8 @@
 import Link from 'next/link';
+import { BsGithub } from 'react-icons/bs';
 
 import LangButton from '../elements/LangButton';
 import ThemeButton from '../elements/ThemeButton';
-
-const navigations = [
-  { label: 'Home', path: '/' },
-  { label: 'About', path: '/' },
-];
 
 export default function Header() {
   return (
@@ -14,16 +10,16 @@ export default function Header() {
       <Link href="/">
         <a className="text-xl">Next.js</a>
       </Link>
-      <nav className="flex grow items-center justify-center space-x-10">
-        {navigations.map(nav => (
-          <Link key={nav.label} href={nav.path}>
-            <a className="hover:opacity-75">{nav.label}</a>
-          </Link>
-        ))}
-      </nav>
       <nav className="flex items-center space-x-6">
         <ThemeButton className="text-xl hover:opacity-75" />
         <LangButton className="text-xl hover:opacity-75" />
+        <a
+          href="https://github.com/derodero24"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsGithub />
+        </a>
       </nav>
     </header>
   );
