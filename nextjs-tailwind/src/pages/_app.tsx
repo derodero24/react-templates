bf6b7b138fa2dd6bc08cfa74a9730a01f2d0c093
derodero24 <k.itsh20524abc@gmail.com>
2022-12-14 11:28:34 +0900

    define 1vh by innerHeight (for mobile)

--- nextjs-tailwind/src/pages/_app.tsx
+++ nextjs-tailwind/src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/global.scss';
 
 import { ThemeProvider } from 'next-themes';
+import { useEffect } from 'react';
 
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  // define 1vh
+  useEffect(() => {
+    const vh = (typeof window === 'undefined' ? 0 : window.innerHeight) * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
