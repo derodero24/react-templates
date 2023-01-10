7159ff2251b060be0e4524995eeb43437ac5d31a
derodero24 <k.itsh20524abc@gmail.com>
2023-01-10 11:33:31 +0900

    add defaultTheme font

--- nextjs-tailwind/tailwind.config.js
+++ nextjs-tailwind/tailwind.config.js
@@ -1,10 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Noto Sans JP'],
+        sans: ['Noto Sans JP', ...defaultTheme.fontFamily.sans],
       },
     },
   },
